bc2bacec39ddf60796cae075f9d32b406be1d8d7
MarkoGlamocak <glamocakmarko99@gmail.com>
2021-05-19 20:01:30 -0500

    Added Welcome Scene and Created a Scene Map Commit

--- JavaFXCheckersGame/src/main/java/App.java
+++ JavaFXCheckersGame/src/main/java/App.java
@@ -1,25 +1,51 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.HashMap;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    @Override
-    public void start(Stage stage) {
+    private HashMap<String, Scene> sceneMap;
 
-        var label = new Label("Hello, JavaFX ");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    // Welcome Scene Data Fields
+    private Button playGameButton;
 
     public static void main(String[] args) {
         launch();
     }
 
+    @Override
+    public void start(Stage stage) {
+        sceneMap = new HashMap<>();
+        sceneMap.put("welcome", welcomeScene());
+        sceneMap.put("game", gameScene());
+
+        playGameButton.setOnAction(e -> stage.setScene(sceneMap.get("game")));
+
+        stage.setScene(sceneMap.get("welcome"));
+        stage.show();
+    }
+
+    public Scene welcomeScene() {
+        Label welcomeLabel = new Label("The Game of Checkers");
+        playGameButton = new Button("Play Game");
+        VBox root = new VBox(welcomeLabel, playGameButton);
+        root.setAlignment(Pos.CENTER);
+        return new Scene(root, 900, 900);
+    }
+
+    public Scene gameScene() {
+        Label gameLabel = new Label("This is the game scene");
+        VBox root = new VBox(gameLabel);
+        return new Scene(root, 700, 700);
+    }
 }
