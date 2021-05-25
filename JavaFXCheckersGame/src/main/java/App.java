659be3a43b3fe9acf4b704e786122f6bb26ac673
MarkoGlamocak <glamocakmarko99@gmail.com>
2021-05-25 17:00:21 -0500

    Created GameBoard

--- JavaFXCheckersGame/src/main/java/App.java
+++ JavaFXCheckersGame/src/main/java/App.java
@@ -1,10 +1,14 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.HashMap;
@@ -15,9 +19,13 @@ import java.util.HashMap;
 public class App extends Application {
 
     private HashMap<String, Scene> sceneMap;
+    private GameButton[][] gameButtonMatrix;
 
-    // Welcome Scene Data Fields
+    // Welcome Scene Data Members
     private Button playGameButton;
+
+    // Game Scene Data Members
+    private GridPane gameBoard;
 
     public static void main(String[] args) {
         launch();
@@ -28,6 +36,8 @@ public class App extends Application {
         sceneMap = new HashMap<>();
         sceneMap.put("welcome", welcomeScene());
         sceneMap.put("game", gameScene());
+
+        gameButtonMatrix = new GameButton[8][8];
 
         playGameButton.setOnAction(e -> stage.setScene(sceneMap.get("game")));
 
@@ -44,8 +54,32 @@ public class App extends Application {
     }
 
     public Scene gameScene() {
-        Label gameLabel = new Label("This is the game scene");
-        VBox root = new VBox(gameLabel);
+        gameBoard = new GridPane();
+        addGrid(gameBoard);
+        VBox root = new VBox(gameBoard);
+        root.setAlignment(Pos.CENTER);
         return new Scene(root, 700, 700);
+    }
+
+    // This function adds game buttons to the GridPane layout and attaches event handlers to each button, so that they are intractable with.
+    public void addGrid(GridPane grid) {
+        int color = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                GameButton gb = new GameButton(j, i, color);
+                if (color == 0) {
+                    color = 1;
+                } else {
+                    color = 0;
+                }
+                gb.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent event) {
+                        // Nothing For Now!
+                    }
+                });
+                grid.add(gb, i, j); // Adds GameButton to GridPane
+                gameButtonMatrix[j][i] = gb; // Adds GameButton to 2D Matrix Data Structure
+            }
+        }
     }
 }
